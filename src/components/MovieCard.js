3bb576e933a3f76633a4fcbf66e8c617a0fbbeec
Chepkoech1 <sandrachepkoech60@gmail.com>
2023-01-16 10:59:43 +0300

    passed in hard-coded data

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -7,9 +7,9 @@ function MovieCard({
 }) {
   return (
     <div className="movie-card">
-      <img src={posterSrc} alt={title} />
-      <h2>{title}</h2>
-      <small>{genres.join(", ")}</small>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM" alt="Mad Max: Fury Road" />
+      <h2>Mad Max: Fury Road</h2>
+      <small>Genres: Action, Adventure, Science Fiction, Thriller</small>
     </div>
   );
 }
